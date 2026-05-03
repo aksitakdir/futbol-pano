8128b588f5852a8a02984a3977eb96841b7e3812
aksitakdir <ozgunozsoy@gmail.com>
2026-05-03 20:47:22 +0300

    feat: next/image remote patterns eklendi

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sofifa.net",
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "cdn.sofifa.net" },
+      { protocol: "https", hostname: "*.supabase.co" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
   },
 };
